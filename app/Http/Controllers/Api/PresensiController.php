4f9a0e5d5b7530aa1cfd83afddbf79e9329ab5a5
adipramandev <herryprasetyo.dev@gmail.com>
2023-11-11 13:01:06 +0700

    tambahan

--- app/Http/Controllers/Api/PresensiController.php
+++ app/Http/Controllers/Api/PresensiController.php
@@ -5,40 +5,44 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Presensi;
+use Auth;
 use Carbon\Carbon;
+use DB;
+use stdClass;
+date_default_timezone_set("Asia/Jakarta");
 
 class PresensiController extends Controller
 {
         //getPresensi
         function getPresensis()
-    {
-        $presensis = Presensi::where('user_id', Auth::user()->id)->get();
-        foreach($presensis as $item) {
-            if ($item->tanggal == date('Y-m-d')) {
-                $item->is_hari_ini = true;
-            } else {
-                $item->is_hari_ini = false;
+        {
+            $presensis = Presensi::where('user_id', Auth::user()->id)->get();
+            foreach($presensis as $item) {
+                if ($item->tanggal == date('Y-m-d')) {
+                    $item->is_hari_ini = true;
+                } else {
+                    $item->is_hari_ini = false;
+                }
+                $datetime = Carbon::parse($item->tanggal)->locale('id');
+                $masuk = Carbon::parse($item->masuk)->locale('id');
+                $pulang = Carbon::parse($item->pulang)->locale('id');
+
+                $datetime->settings(['formatFunction' => 'translatedFormat']);
+                $masuk->settings(['formatFunction' => 'translatedFormat']);
+                $pulang->settings(['formatFunction' => 'translatedFormat']);
+
+                $item->tanggal = $datetime->format('l, j F Y');
+                $item->masuk = $masuk->format('H:i');
+                $item->pulang = $pulang->format('H:i');
             }
-            $datetime = Carbon::parse($item->tanggal)->locale('id');
-            $masuk = Carbon::parse($item->masuk)->locale('id');
-            $pulang = Carbon::parse($item->pulang)->locale('id');
 
-            $datetime->settings(['formatFunction' => 'translatedFormat']);
-            $masuk->settings(['formatFunction' => 'translatedFormat']);
-            $pulang->settings(['formatFunction' => 'translatedFormat']);
+            return response()->json([
+                'success' => true,
+                'data' => $presensis,
+                'message' => 'Sukses menampilkan data'
+            ]);
 
-            $item->tanggal = $datetime->format('l, j F Y');
-            $item->masuk = $masuk->format('H:i');
-            $item->pulang = $pulang->format('H:i');
+
         }
-
-        return response()->json([
-            'success' => true,
-            'data' => $presensis,
-            'message' => 'Sukses menampilkan data'
-        ]);
-
-
-    }
 
 }
